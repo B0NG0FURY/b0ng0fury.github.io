53424034c905f90a825ce2d9f67c441f2afc8e0e
bongofury <jtroutmanman@gmail.com>
2022-01-21 17:47:29 -0600

    created Welcome component to introduce myself

---
+++ src/components/Welcome.js
@@ -0,0 +1,8 @@
+export default function Welcome() {
+    return (
+        <section>
+            <h1>Hi, I'm John Troutman</h1>
+            <p>I like to code things</p>
+        </section>
+    )
+}
