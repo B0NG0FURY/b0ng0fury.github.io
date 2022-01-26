010665cae1fd2018a1347bc17d9e74417b26345d
bongofury <jtroutmanman@gmail.com>
2022-01-25 22:29:56 -0600

    framed out position for program language icons in welcome component.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,6 +3,12 @@ export default function Welcome() {
         <article className="welcome">
             <h1>Hi, I'm John Troutman</h1>
             <p>I like to code things</p>
+
+            <section class="code-icons">
+                <p>-img-<br></br>JavaScript</p>
+                <p>-img-<br></br>React</p>
+                <p>-img-<br></br>Ruby</p>
+            </section>
         </article>
     )
 }
