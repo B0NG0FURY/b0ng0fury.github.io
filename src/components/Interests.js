236fe2873e2706398f7f12cde9f68110119165d1
bongofury <jtroutmanman@gmail.com>
2022-02-04 16:08:01 -0600

    carousel images now properly situated. added placeholder images for the time being.

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,35 +4,35 @@ export default function Interests() {
     return (
         <Carousel interval="10000" className="interest-slides">
             <Carousel.Item>
-                <img className="d-block w-100" alt="Music"/>
+                <img className="d-block w-100" alt="Music" src="http://placehold.it/600x400"/>
                 <Carousel.Caption>
                     <h5>Music</h5>
                     <p>Text goes here...</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img className="d-block w-100" alt="Movies"/>
+                <img className="d-block w-100" alt="Movies" src="http://placehold.it/600x400"/>
                 <Carousel.Caption>
                     <h5>Movies</h5>
                     <p>Text goes here...</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img className="d-block w-100" alt="Onewheel"/>
+                <img className="d-block w-100" alt="Onewheel" src="http://placehold.it/600x400"/>
                 <Carousel.Caption>
                     <h5>Onewheel</h5>
                     <p>Text goes here...</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img className="d-block w-100" alt="Travel"/>
+                <img className="d-block w-100" alt="Travel" src="http://placehold.it/600x400"/>
                 <Carousel.Caption>
                     <h5>Travel</h5>
                     <p>Text goes here...</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img className="d-block w-100" alt="Video Games"/>
+                <img className="d-block w-100" alt="Video Games" src="http://placehold.it/600x400"/>
                 <Carousel.Caption>
                     <h5>Video Games</h5>
                     <p>Text goes here...</p>
