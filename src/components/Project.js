ff64839d4f162acf1bde0414789e740460a66036
bongofury <jtroutmanman@gmail.com>
2022-02-08 19:39:16 -0600

    links in project cards now open in new tab. added margin between project cards and to images within the cards.

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,17 +4,31 @@ import Card from 'react-bootstrap/Card';
 export default function Project(props) {
     return (
         <Accordion>
-                <Card>
+                <Card className="project-card">
                     <Card.Header>{props.project.name}</Card.Header>
-                    <Card.Img variant="top" src={props.project.img}/>
+                    <Card.Img  className="project-img" variant="top" src={props.project.img}/>
                     <Accordion.Header>Info</Accordion.Header>
                     <Accordion.Body>
                         <Card.Body>
                             <Card.Text>
                                 <section>
                                     <p>{props.project.about}</p>
-                                    <a href={props.project.demo}>Demo Video</a>
-                                    <a href={props.project.github}>Github</a>
+                                    <a 
+                                        className="float-left"
+                                        href={props.project.demo}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Demo Video
+                                    </a>
+                                    <a 
+                                        className="float-right"
+                                        href={props.project.github}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Github
+                                    </a>
                                 </section>
                             </Card.Text>
                         </Card.Body>
