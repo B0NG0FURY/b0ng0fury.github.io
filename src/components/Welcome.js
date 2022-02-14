83b97652e275d4d1f84d1d773f28517b5a9d882a
bongofury <jtroutmanman@gmail.com>
2022-02-13 18:41:12 -0600

    added image to Welcome component. adjusted margin of same component.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,10 @@
 import icons from '../icons';
+import self from '../self.jpg';
 
 export default function Welcome() {
     return (
         <section className="welcome">
+            <img src={self} id="self-img" />
             <h1>Hi, I'm John Troutman</h1>
             <p>I like to code things</p>
 
