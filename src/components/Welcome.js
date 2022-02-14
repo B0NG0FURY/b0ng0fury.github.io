80927aaa3cfbd91f6ff62ef08d922ca597336fe7
bongofury <jtroutmanman@gmail.com>
2022-02-13 18:27:12 -0600

    added programming language icons to Welcome component with image src locations put in an object in icons.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,14 @@
+import icons from '../icons';
+
 export default function Welcome() {
     return (
-        <article className="welcome">
+        <section className="welcome">
             <h1>Hi, I'm John Troutman</h1>
             <p>I like to code things</p>
 
             <section class="code-icons">
-                <p>-img-<br></br>JavaScript</p>
-                <p>-img-<br></br>React</p>
-                <p>-img-<br></br>Ruby</p>
+                {Object.values(icons).map(src => <img src={src} />)}
             </section>
-        </article>
+        </section>
     )
 }
---
+++ src/icons.js
@@ -0,0 +1,10 @@
+const icons = {
+    javascript: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    react: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    ruby: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg",
+    rails: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain.svg",
+    html: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg",
+    css: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg"
+}
+
+export default icons;
