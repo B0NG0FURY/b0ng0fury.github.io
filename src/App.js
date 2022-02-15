af2e0ae3670be3fe2737839097bca63be5649783
bongofury <jtroutmanman@gmail.com>
2022-02-14 21:57:07 -0600

    added project pictures for top-five and trivia-time to Project component.

--- src/App.js
+++ src/App.js
@@ -10,22 +10,6 @@ import Portfolio from './components/Portfolio';
 function App() {
   const [active, setActive] = useState("Welcome");
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div className="page">
       <Nav setActive={setActive} active={active} />
       <section className="main">
--- src/projects.js
+++ src/projects.js
@@ -2,14 +2,14 @@ const projects = [
     {
         name: "Trivia Time",
         about: "Trivia app built with a React frontend and Rails backend. A postgreSQL database holds the data. Allows the user to create custom games based on the category and number of questions of their choosing.",
-        img: "http://placehold.it/400x200", 
+        img: "/trivia-time.png", 
         demo: "https://drive.google.com/file/d/1f5USsq7zOqadGm6V_VYepPUtfpc35E_k/view",
         github: "https://github.com/B0NG0FURY/trivia-time"
     },
     {
         name: "Top Five",
         about: "List app giving user ability to make simple ranked lists and categorize them. Lists can be easily edited with ranks able to be quickly and dynamically changed. Uses a pure ES6 JavaScript frontend and a Rails API backend to save the user's lists.",
-        img: "http://placehold.it/400x200",
+        img: "/top-five.png",
         demo: "https://drive.google.com/file/d/1ULwBVoa0-CAlTt2rZW4ONtAnn-jUZfm6/view",
         github: "https://github.com/B0NG0FURY/top-five"
     },
