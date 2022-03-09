c7fd66f41cbcd57d281e25818679c16b038cb222
bongofury <jtroutmanman@gmail.com>
2022-03-09 17:28:38 -0600

    added image assets for movie collection and music release projects. added info for movie collection app. improved wording and descriptions of other projects.

--- src/projects.js
+++ src/projects.js
@@ -8,17 +8,24 @@ const projects = [
     },
     {
         name: "Top Five",
-        about: "List app giving user ability to make simple ranked lists and categorize them. Lists can be easily edited with ranks able to be quickly and dynamically changed. Uses a pure ES6 JavaScript frontend and a Rails API backend to save the user's lists.",
+        about: "List app giving user ability to make simple ranked lists and categorize them. Lists can be easily edited, filtered by category, and item ranks quickly and dynamically changed. Uses a pure JavaScript frontend and a Rails API backend to save the user's lists to a postgreSQL database.",
         img: "/top-five.png",
         demo: "https://drive.google.com/file/d/1ULwBVoa0-CAlTt2rZW4ONtAnn-jUZfm6/view",
         github: "https://github.com/B0NG0FURY/top-five"
     },
     {
         name: "Beer Enthusiast",
-        about: "Beer Enthusiast is a full stack application built with Rails that allows the user to explore beers, find information, and read reviews from those who have tried them. The user can also rate beers and keep track of what they have liked and what they didn't. Application uses a MVC structure and omniauth sign in capability with Google.",
+        about: "Beer Enthusiast is a full stack application built with Rails that allows the user to explore beers, find information, and read reviews from those who have tried them. The user can also rate beers and keep track of what they have liked and what they didn't. Application uses a MVC structure, omniauth sign in capability with Google, and sqlite database.",
         img: "/beer-enthusiast-main.png",
         demo: "https://drive.google.com/file/d/1_WXM-vchO-PS27p_vsi5p3eG6cxeZjjx/view",
         github: "https://github.com/B0NG0FURY/beer_enthusiast"
+    },
+    {
+        name: "Movie Collection Database",
+        about: "Application built with Sinatra to catalog the user's personal movie collection. Movie's can be categorized and viewed by title, director, genre, and format. Features a sqlite database to store the user's collection. Bcrypt and sessions handle authentication for sign up and login.",
+        img: "/movie-app.png",
+        demo: "https://drive.google.com/file/d/1M-Ise8Qp_8wf_oFlV09f08J7QGhYOmcN/view?usp=sharing",
+        github: "https://github.com/B0NG0FURY/movie_collection"
     }
 ];
 
